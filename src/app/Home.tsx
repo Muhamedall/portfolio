4a4ffb50c7ffd0ee9d113baa71279073ce063d89
Muhamedall <mohamedallaoui425@gmail.com>
2026-07-01 01:28:17 +0100

    UP4:PORTFOLIO

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -57,7 +57,7 @@ export default function Page() {
               </a>
 
               <a
-                href="/assets/RESUME-DEVELOPER-MR-ALLAOUI.pdf"
+                href="/assets/RESUME-DEVELOPER-MR-ALLAOUI.pdf" 
                 download
                 className="bg-gray-800 hover:bg-gray-900 text-white font-semibold py-3 px-6 rounded-lg transition duration-300"
               >
